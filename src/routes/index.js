51eadd90cacce1ca968d288063b189d6ab444166
Jian <jianhui9998@gmail.com>
2022-01-17 05:38:08 +0800

    feature: add ejs demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,36 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    tagMe: true,
+    blogList: [
+      {
+        id: 1,
+        title: 'a'
+      },
+      {
+        id: 2,
+        title: 'b'
+      },
+      {
+        id: 3,
+        title: 'c'
+      },
+    ]
   })
-})
-
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async(ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
   }
 })
 
